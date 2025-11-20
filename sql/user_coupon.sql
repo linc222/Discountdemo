2f94a5df83cd0e041d6ca2f5bc9321ff1de603aa
linc222 <20730390666@qq.com>
2025-11-20 20:42:25 +0800

    添加sql

---
+++ sql/user_coupon.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user_coupon`(`id`, `user_id`, `template_id`, `order_id`, `status`, `receive_time`, `use_time`) VALUES (1, 1001, 1, NULL, 2, '2025-11-20 19:39:17', '2025-11-20 19:39:22');
+INSERT INTO `user_coupon`(`id`, `user_id`, `template_id`, `order_id`, `status`, `receive_time`, `use_time`) VALUES (2, 1001, 2, NULL, 2, '2025-11-20 19:40:05', '2025-11-20 19:40:18');
+INSERT INTO `user_coupon`(`id`, `user_id`, `template_id`, `order_id`, `status`, `receive_time`, `use_time`) VALUES (3, 1001, 3, NULL, 2, '2025-11-20 19:40:51', '2025-11-20 19:41:03');
